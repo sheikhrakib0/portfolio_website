0e3f0a01a676cb60c11a5647a82fb102db0c9784
sheikhrakib0 <rakibrakib0179@gmai.com>
2025-07-07 03:57:41 +0600

    app.py update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)  # Allow frontend to call this backend
@@ -34,4 +35,4 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
