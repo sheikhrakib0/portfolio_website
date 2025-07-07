d71dd9c4b85e7fe4aec0ce05364265904a57c91d
sheikhrakib0 <rakibrakib0179@gmai.com>
2025-07-07 14:57:53 +0600

    model upgrade

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import joblib
 import pandas as pd
 import os
+from sklearn.compose import ColumnTransformer
 
 app = Flask(__name__)
 CORS(app)  # Allow frontend to call this backend
